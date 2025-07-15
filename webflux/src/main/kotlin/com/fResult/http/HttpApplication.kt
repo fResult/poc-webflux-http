ec7de409673661898e711ce1e0cbebab29954f83
fResult <styxmaz@gmail.com>
2025-07-16 02:50:28 +0700

    Remove unused class.

--- webflux/src/main/kotlin/com/fResult/http/HttpApplication.kt
+++ webflux/src/main/kotlin/com/fResult/http/HttpApplication.kt
@@ -2,8 +2,6 @@ package com.fResult.http
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,14 +10,4 @@ class HttpApplication
 
 fun main(args: Array<String>) {
   runApplication<HttpApplication>(*args)
-}
-
-@RestController
-@RequestMapping("/greets")
-class GreetController {
-
-  @GetMapping("/{name}")
-  fun greet(@PathVariable name: String): String {
-    return "Hello, $name!"
-  }
 }
