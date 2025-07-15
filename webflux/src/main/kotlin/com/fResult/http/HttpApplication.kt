2bb9c68c35c1644f2a70b3225270deb5008e8dcd
fResult <styxmaz@gmail.com>
2025-07-16 02:09:37 +0700

    Test `/greets` endpoint works.

--- webflux/src/main/kotlin/com/fResult/http/HttpApplication.kt
+++ webflux/src/main/kotlin/com/fResult/http/HttpApplication.kt
@@ -2,10 +2,24 @@ package com.fResult.http
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class HttpApplication
 
 fun main(args: Array<String>) {
   runApplication<HttpApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/greets")
+class GreetController {
+
+  @GetMapping("/{name}")
+  fun greet(@PathVariable name: String): String {
+    return "Hello, $name!"
+  }
 }
