869d59844401e30f2d21b60f5e0d336b32d46c6b
fResult <styxmaz@gmail.com>
2025-07-16 01:29:58 +0700

    1st commit.

---
+++ src/test/kotlin/com/fResult/http/HttpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fResult.http
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HttpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
