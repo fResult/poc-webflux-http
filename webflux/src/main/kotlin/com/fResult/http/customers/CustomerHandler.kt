2a0e8bc54f58741f49850c8b399822a6c25569d7
fResult <styxmaz@gmail.com>
2025-07-19 12:45:44 +0700

    Fix error `'producer' type is unknown to ReactiveAdapterRegistry`.

--- webflux/src/main/kotlin/com/fResult/http/customers/CustomerHandler.kt
+++ webflux/src/main/kotlin/com/fResult/http/customers/CustomerHandler.kt
@@ -1,17 +1,25 @@
 package com.fResult.http.customers
 
+import org.reactivestreams.Publisher
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @Component
 class CustomerHandler(private val repository: CustomerRepository) {
   fun handleFindAll(ignored: ServerRequest): Mono<ServerResponse> =
     repository.findAll()
       .collectList()
-      .flatMap(::toResponseWithBody)
+      .flatMap(::listToOkResponse)
 }
 
-private inline fun <reified T> toResponseWithBody(responseBody: List<T>): Mono<ServerResponse> =
+private inline fun <reified T> listToOkResponse(responseBody: List<T>): Mono<ServerResponse> =
+  responseBody.toMono()
+    .flatMapMany(Flux<T>::fromIterable)
+    .let(::respondWithOkStreamBody)
+
+private inline fun <reified T> respondWithOkStreamBody(responseBody: Publisher<T>): Mono<ServerResponse> =
   ServerResponse.ok().body(responseBody, T::class.java)
