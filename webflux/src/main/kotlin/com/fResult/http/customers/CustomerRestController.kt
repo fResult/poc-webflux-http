c284d5e3896398680c166e8f9cf9daa6472d023d
fResult <styxmaz@gmail.com>
2025-07-19 15:18:40 +0700

    Add throw Mono with response status 404 when customer not found in Rest Controller.

--- webflux/src/main/kotlin/com/fResult/http/customers/CustomerRestController.kt
+++ webflux/src/main/kotlin/com/fResult/http/customers/CustomerRestController.kt
@@ -1,7 +1,9 @@
 package com.fResult.http.customers
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
 import java.net.URI
 
@@ -13,6 +15,7 @@ class CustomerRestController(private val repository: CustomerRepository) {
 
   @GetMapping("/{id}")
   fun byId(@PathVariable id: String) = repository.findById(id)
+    .switchIfEmpty(Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND)))
 
   @PostMapping
   fun create(@RequestBody customer: Customer): Mono<ResponseEntity<Customer>> = repository.save(customer)
