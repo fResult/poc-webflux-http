b22faee2d349e6a40bb33be1d5f7782313dca267
fResult <styxmaz@gmail.com>
2025-07-20 00:46:34 +0700

    Create `NestedFunctionalEndpointConfiguration#nested` with GET `/nested/:pv` endpoint.

---
+++ webflux/src/main/kotlin/com/fResult/http/routes/NestedFunctionalEndpointConfiguration.kt
@@ -0,0 +1,26 @@
+package com.fResult.http.routes
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+import java.nio.charset.Charset
+
+@Configuration
+class NestedFunctionalEndpointConfiguration {
+  @Bean
+  fun nested(nestedHandler: NestedHandler): RouterFunction<ServerResponse> {
+    val jsonRP = RequestPredicates.accept(MediaType.APPLICATION_JSON)
+      .or(RequestPredicates.accept(MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8"))))
+
+    return RouterFunctions.route()
+      .nest(RequestPredicates.path("/nested")) { builder ->
+        builder.nest(jsonRP) { nestedBuilder ->
+          nestedBuilder.GET("/{pv}", nestedHandler::pathVariable)
+        }
+      }.build()
+  }
+}
