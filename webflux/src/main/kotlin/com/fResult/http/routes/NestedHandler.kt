779ff3a8578bb43174f68d735d2bee5c13da8440
fResult <styxmaz@gmail.com>
2025-07-20 12:37:48 +0700

    Create `NestedHandler#sse` method.

--- webflux/src/main/kotlin/com/fResult/http/routes/NestedHandler.kt
+++ webflux/src/main/kotlin/com/fResult/http/routes/NestedHandler.kt
@@ -1,5 +1,7 @@
 package com.fResult.http.routes
 
+import com.fResult.http.utils.IntervalMessageProducer
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -15,6 +17,11 @@ class NestedHandler {
 
   fun noPathVariable(ignored: ServerRequest): Mono<ServerResponse> =
     greet().let { ServerResponse.ok().bodyValue(it) }
+
+  fun sse(request: ServerRequest): Mono<ServerResponse> =
+    ServerResponse.ok()
+      .contentType(MediaType.TEXT_EVENT_STREAM)
+      .body(IntervalMessageProducer.produce(), String::class.java)
 
   private fun greet(name: String? = null): Map<String, String> =
     mapOf("message" to "Hello, ${name ?: "World"}!")
