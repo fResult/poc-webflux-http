68a1434c291671e1ec89e4e2f61c79bee7748a97
fResult <styxmaz@gmail.com>
2025-07-20 13:28:39 +0700

    Create `CustomRoutePredicates#handle` lambda.

---
+++ webflux/src/main/kotlin/com/fResult/http/routes/CustomRoutePredicates.kt
@@ -0,0 +1,17 @@
+package com.fResult.http.routes
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.HandlerFunction
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.kotlin.core.publisher.toMono
+
+@Configuration
+class CustomRoutePredicates {
+  val handle: HandlerFunction<ServerResponse> = HandlerFunction { request: ServerRequest ->
+    request.queryParam("name")
+      .toMono()
+      .map { "Hello, ${it.orElse("World")}" }
+      .flatMap { ServerResponse.ok().bodyValue(it) }
+  }
+}
