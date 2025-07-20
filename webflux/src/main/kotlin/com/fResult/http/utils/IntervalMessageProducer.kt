28943be1e2549ac92776916dc9e079a0c9b76d62
fResult <styxmaz@gmail.com>
2025-07-20 12:36:50 +0700

    Create `IntervalMessageProducer` open class.

---
+++ webflux/src/main/kotlin/com/fResult/http/utils/IntervalMessageProducer.kt
@@ -0,0 +1,25 @@
+package com.fResult.http.utils
+
+import reactor.core.publisher.Flux
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+open class IntervalMessageProducer {
+  companion object {
+    fun produce(count: Int): Flux<String> {
+      return produce().take(count.toLong())
+    }
+
+    fun produce(): Flux<String> {
+      return produceCountAndString().map(CountAndString::message)
+    }
+
+    private fun produceCountAndString(): Flux<CountAndString> {
+      val counter = AtomicLong()
+
+      return Flux.interval(1.seconds.toJavaDuration())
+        .map { CountAndString(counter.incrementAndGet().toInt()) }
+    }
+  }
+}
