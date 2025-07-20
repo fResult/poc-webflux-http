3e7fbf3058b92c13621639a1af904579068dd327
fResult <styxmaz@gmail.com>
2025-07-20 17:17:34 +0700

    Create `sup` test method.

---
+++ webflux/src/test/kotlin/com/fResult/http/routes/SimpleFunctionalEndpointConfigurationTest.kt
@@ -0,0 +1,24 @@
+package com.fResult.http.routes
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@WebFluxTest(SimpleFunctionalEndpointConfiguration::class, GreetingsHandlerFunction::class)
+class SimpleFunctionalEndpointConfigurationTest {
+  @Autowired
+  lateinit var webTestClient: WebTestClient
+
+  @Test
+  fun sup(): Unit = parameterizeTestString("/sup", "Hodor!")
+
+  private fun parameterizeTestString(path: String, expectedResult: String) {
+    webTestClient.get()
+      .uri(path)
+      .exchange()
+      .expectBody(String::class.java)
+      .value {str -> assertEquals(expectedResult, str) }
+  }
+}
