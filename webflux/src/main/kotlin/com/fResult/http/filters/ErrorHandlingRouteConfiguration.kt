b93643ab314e06657fbad7ddced0f25a67b66407
fResult <styxmaz@gmail.com>
2025-07-22 23:59:10 +0700

    Create GET `/errors/customers/:id` endpoint to try Webflux error hanling.

--- webflux/src/main/kotlin/com/fResult/http/filters/ErrorHandlingRouteConfiguration.kt
+++ webflux/src/main/kotlin/com/fResult/http/filters/ErrorHandlingRouteConfiguration.kt
@@ -1,12 +1,9 @@
 package com.fResult.http.filters
 
+import com.fResult.http.customers.Customer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyValueAndAwait
-import org.springframework.web.reactive.function.server.coRouter
+import org.springframework.web.reactive.function.server.*
 
 @Configuration
 class ErrorHandlingRouteConfiguration {
@@ -14,27 +11,50 @@ class ErrorHandlingRouteConfiguration {
   fun errors(): RouterFunction<ServerResponse> = coRouter {
     GET("/errors").nest {
       GET("products/{id}", ::findProductById)
+      GET("customers/{id}", ::findCustomerById)
     }
   }.filter { request, next ->
-    next.handle(request).onErrorResume(ProductNotFoundException::class.java) { ex ->
+    next.handle(request).onErrorResume(ElementNotFoundException::class.java) { ex ->
       ServerResponse.notFound().build()
     }
   }
 
   private suspend fun findProductById(request: ServerRequest): ServerResponse {
+    val productId = request.pathVariable("id")
+
+    return if (setOf("1", "2").contains(productId)) {
+      throw ProductNotFoundException("Product with ID [$productId] not found")
+    } else {
+      ServerResponse.ok().bodyValueAndAwait(Product(productId))
+    }
+  }
+
+  private suspend fun findCustomerById(request: ServerRequest): ServerResponse {
     val customerId = request.pathVariable("id")
 
     return if (setOf("1", "2").contains(customerId)) {
-      throw ProductNotFoundException("Customer with ID [$customerId] not found")
+      throw CustomerNotFoundException("Customer with ID [$customerId] not found")
     } else {
-      ServerResponse.ok().bodyValueAndAwait(Product(customerId))
+      ServerResponse.ok().bodyValueAndAwait(Customer(customerId, "Customer Name $customerId"))
     }
   }
 }
 
 data class Product(val id: String)
 
-class ProductNotFoundException : RuntimeException {
+class ProductNotFoundException : ElementNotFoundException {
+  constructor(message: String) : super(message)
+  constructor(cause: Throwable) : super(cause)
+  constructor(message: String, cause: Throwable) : super(message, cause)
+}
+
+class CustomerNotFoundException : ElementNotFoundException {
+  constructor(message: String) : super(message)
+  constructor(cause: Throwable) : super(cause)
+  constructor(message: String, cause: Throwable) : super(message, cause)
+}
+
+open class ElementNotFoundException : RuntimeException {
   constructor(message: String) : super(message)
   constructor(cause: Throwable) : super(cause)
   constructor(message: String, cause: Throwable) : super(message, cause)
