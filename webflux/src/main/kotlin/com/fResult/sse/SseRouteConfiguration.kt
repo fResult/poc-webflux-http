45d0e38820dd379e536cf1f80477854e40dead11
fResult <styxmaz@gmail.com>
2025-07-24 00:21:14 +0700

    Create GET `/sse/:count` endpoint.

--- webflux/src/main/kotlin/com/fResult/sse/SseRouteConfiguration.kt
+++ webflux/src/main/kotlin/com/fResult/sse/SseRouteConfiguration.kt
@@ -7,15 +7,18 @@ import org.apache.logging.log4j.Logger
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.*
 
 @Configuration
 class SseRouteConfiguration {
   companion object {
     private const val COUNT_PATH_VARIABLE: String = "count"
     private val log: Logger = LogManager.getLogger(SseRouteConfiguration::class.java)
+  }
+
+  @Bean
+  fun routes(): RouterFunction<ServerResponse> = coRouter {
+    GET("/sse/{$COUNT_PATH_VARIABLE}", ::handleSse)
   }
 
   private suspend fun handleSse(request: ServerRequest): ServerResponse {
