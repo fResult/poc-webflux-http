f9d0487b88d40e95024fe6732e2d3219f79e2c02
fResult <styxmaz@gmail.com>
2025-07-24 23:39:25 +0700

    Prepare `echoWsh` method as a `WebSocketHandler`.

--- webflux/src/main/kotlin/com/fResult/ws/echo/EchoWebSocketConfiguration.kt
+++ webflux/src/main/kotlin/com/fResult/ws/echo/EchoWebSocketConfiguration.kt
@@ -1,12 +1,38 @@
 package com.fResult.ws.echo
 
+import com.fResult.utils.IntervalMessageProducer
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.socket.WebSocketHandler
+import org.springframework.web.reactive.socket.WebSocketMessage
+import reactor.core.publisher.SignalType
+import reactor.core.scheduler.Schedulers
 
 @Configuration
 class EchoWebSocketConfiguration {
   companion object {
     private val log: Logger = LogManager.getLogger(EchoWebSocketConfiguration::class.java)
+  }
+
+  @Bean
+  fun echoWsh(): WebSocketHandler = WebSocketHandler { session ->
+    val outbound = IntervalMessageProducer.produce()
+      .doOnNext(log::info)
+      .map(session::textMessage)
+      .doFinally { signalType -> log.info("outbound connection: {}", signalType) }
+
+    val inbound = session.receive()
+      .map(WebSocketMessage::getPayloadAsText)
+      .publishOn(Schedulers.boundedElastic())
+      .doFinally { signalType ->
+        log.info("Inbound connection: {}", signalType)
+        if (signalType == SignalType.ON_COMPLETE) {
+          session.close().subscribe()
+        }
+      }.doOnNext(log::info)
+
+    session.send(outbound).and(inbound)
   }
 }
