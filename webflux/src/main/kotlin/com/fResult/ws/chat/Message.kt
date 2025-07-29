d2431efef82023e57c080da4c185fa5cb580449f
fResult <styxmaz@gmail.com>
2025-07-29 18:33:10 +0700

    Update `Message` data structure from class to data class.

--- webflux/src/main/kotlin/com/fResult/ws/chat/Message.kt
+++ webflux/src/main/kotlin/com/fResult/ws/chat/Message.kt
@@ -2,4 +2,4 @@ package com.fResult.ws.chat
 
 import java.time.Instant
 
-class Message(val clientId: String, val text: String, val `when`: Instant)
+data class Message(val clientID: String, val text: String, val `when`: Instant)
