b1d5cacbd8ca2023efc911b64d1de7e9307ed0d0
fResult <styxmaz@gmail.com>
2025-08-02 21:43:39 +0700

    Create `controller()` parameterized test method.

--- servlets/src/test/kotlin/com/fresult/servlets/TomcatWebfluxApplicationTests.kt
+++ servlets/src/test/kotlin/com/fresult/servlets/TomcatWebfluxApplicationTests.kt
@@ -1,13 +1,31 @@
 package com.fresult.servlets
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
-class ServletsApplicationTests {
+@WebFluxTest
+@Import(GreetingsRouteConfiguration::class)
+class TomcatWebfluxApplicationTests {
+  @Autowired
+  private lateinit var webTestClient: WebTestClient
 
-  @Test
-  fun contextLoads() {
+  @ParameterizedTest
+  @CsvSource(*["functional:John", "controller:Jane"], delimiter = ':')
+  fun controller(from: String, name: String) {
+    webTestClient.get()
+      .uri("/hello/$from/{name}", name)
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectHeader()
+      .contentType(MediaType.APPLICATION_JSON)
+      .expectBody(Greetings::class.java)
+      .value { greetings -> Assertions.assertEquals(greetings?.message, "Hello, $name from $from!") }
   }
-
 }
