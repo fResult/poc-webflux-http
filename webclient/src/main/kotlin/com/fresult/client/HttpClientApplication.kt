28f4c69ede2be68879fdd3e7fe264513c358bd57
fResult <styxmaz@gmail.com>
2025-08-03 00:56:42 +0700

    Enable ClientProperties class as config props.

--- webclient/src/main/kotlin/com/fresult/client/HttpClientApplication.kt
+++ webclient/src/main/kotlin/com/fresult/client/HttpClientApplication.kt
@@ -1,9 +1,11 @@
 package com.fresult.client
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(ClientProperties::class)
 class HttpClientApplication
 
 fun main(args: Array<String>) {
