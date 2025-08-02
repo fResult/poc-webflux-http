abab2ebcb369c0598665590049ccbbd17b4ce42b
fResult <styxmaz@gmail.com>
2025-08-03 00:55:57 +0700

    Create `HttpController` class.

--- webclient/src/main/kotlin/com/fresult/service/HttpController.kt
+++ webclient/src/main/kotlin/com/fresult/service/HttpController.kt
@@ -1,4 +1,28 @@
 package com.fresult.service
 
+import com.fresult.client.Greeting
+import org.reactivestreams.Publisher
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.time.Instant
+import java.util.stream.Stream
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+@RestController
+@RequestMapping("/greet")
 class HttpController {
+  @GetMapping("/single/{name}")
+  fun greetSingle(@PathVariable name: String): Publisher<Greeting> = Mono.just(Greeting(name))
+
+  @GetMapping("/many/{name}")
+  fun greetMany(@PathVariable name: String): Publisher<Greeting> =
+    Flux.fromStream(Stream.generate { greeting(name) })
+      .delayElements(1.seconds.toJavaDuration());
+
+  fun greeting(name: String) = Greeting("Hello, $name @ ${Instant.now()}")
 }
