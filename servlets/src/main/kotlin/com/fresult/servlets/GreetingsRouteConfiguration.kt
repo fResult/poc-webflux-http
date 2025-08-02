fe3085db9e7b366ebc7b10bb529d8fae97feae2c
fResult <styxmaz@gmail.com>
2025-08-02 19:24:44 +0700

    Create `GreetingsRouteConfiguration` class.

---
+++ servlets/src/main/kotlin/com/fresult/servlets/GreetingsRouteConfiguration.kt
@@ -0,0 +1,19 @@
+package com.fresult.servlets
+
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class GreetingsRouteConfiguration {
+  @Bean
+  fun routes(): RouterFunction<ServerResponse> = coRouter {
+    GET("/hello/functional/{name}") { request ->
+      Greetings.greet("functional", request.pathVariable("name"))
+        .flatMap(ServerResponse.ok()::bodyValue).awaitSingle()
+    }
+  }
+}
