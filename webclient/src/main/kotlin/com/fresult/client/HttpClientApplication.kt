00fbf3f2d9555a9d1bb296c6cf6e9597de97fc0d
fResult <styxmaz@gmail.com>
2025-08-03 20:01:35 +0700

    Bootstrap `HttpClientApplication` with `client` profile when App starting.

--- webclient/src/main/kotlin/com/fresult/client/HttpClientApplication.kt
+++ webclient/src/main/kotlin/com/fresult/client/HttpClientApplication.kt
@@ -1,13 +1,19 @@
 package com.fresult.client
 
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties(ClientProperties::class)
 class HttpClientApplication
 
 fun main(args: Array<String>) {
-  runApplication<HttpClientApplication>(*args)
+  SpringApplicationBuilder()
+    .sources(HttpClientApplication::class.java)
+    .profiles("client")
+    .run(*args)
+
+  Thread.sleep(20_000)
 }
