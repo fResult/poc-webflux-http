15edc8f0218f490d37341a1fea2409d8ec80aa04
fResult <styxmaz@gmail.com>
2025-08-03 20:02:02 +0700

    Install `spring-boot-starter-webclient` dependency.

--- webclient/build.gradle.kts
+++ webclient/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
