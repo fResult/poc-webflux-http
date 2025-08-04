aa74c5130a0cd74f63d31708cc5d200717f23790
fResult <styxmaz@gmail.com>
2025-08-05 01:41:50 +0700

    Create `SecurityFilterChain` bean.

--- security/src/main/kotlin/com/fresult/security/SecurityConfiguration.kt
+++ security/src/main/kotlin/com/fresult/security/SecurityConfiguration.kt
@@ -2,9 +2,12 @@ package com.fresult.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfiguration {
@@ -14,6 +17,18 @@ class SecurityConfiguration {
     val user2 = buildUserDetails("KornZilla", "password2", "USER", "ADMIN")
 
     return MapReactiveUserDetailsService(user1, user2)
+  }
+
+  @Bean
+  fun authorization(httpSecurity: ServerHttpSecurity): SecurityFilterChain {
+    return httpSecurity
+      .httpBasic(Customizer.withDefaults())
+      .authorizeExchange { authorizeExchange ->
+        authorizeExchange
+          .pathMatchers("/greetings").authenticated()
+          .anyExchange().permitAll()
+      }.csrf(ServerHttpSecurity.CsrfSpec::disable)
+      .build() as SecurityFilterChain
   }
 
   private fun buildUserDetails(
